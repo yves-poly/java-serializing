d4ba73f62cd08a8b2adf000adbd2d380ef3c7bf6
Yves E. Dossous <yves-eli.dossous@polymtl.ca>
2022-12-02 22:20:34 -0500

    implemented functions load() save() that save and load previous program state from file

--- src/main/java/Game/MedievalGame.java
+++ src/main/java/Game/MedievalGame.java
@@ -1,66 +1,103 @@
 package Game;
 
+import java.io.*;
 import java.util.Scanner;
+
+import static Game.Art.homeScreen;
 
 public class MedievalGame {
 
-  /* Instance Variables */
-  Player player;
-  /* Main Method */
+  /* instance Variables */
+  private Player player;
+
+  // Main method
   public static void main(String[] args) {
-    
+
     Scanner console = new Scanner(System.in);
     MedievalGame game = new MedievalGame();
 
     game.player = game.start(console);
 
-    game.addDelay(500);
+    //game.addDelay(500);
     System.out.println("\nLet's take a quick look at you to make sure you're ready to head out the door.");
     System.out.println(game.player);
 
-    game.addDelay(1000);
+    //game.addDelay(1000);
     System.out.println("\nWell, you're off to a good start, let's get your game saved so we don't lose it.");
     game.save();
 
-    game.addDelay(2000);
+    //game.addDelay(2000);
     System.out.println("We just saved your game...");
     System.out.println("Now we are going to try to load your character to make sure the save worked...");
 
-    game.addDelay(1000);
+    //game.addDelay(1000);
     System.out.println("Deleting character...");
     String charName = game.player.getName();
     game.player = null;
 
-    game.addDelay(1500);
+    //game.addDelay(1500);
     game.player = game.load(charName, console);
     System.out.println("Loading character...");
 
-    game.addDelay(2000);
+    //game.addDelay(2000);
     System.out.println("Now let's print out your character again to make sure everything loaded:");
 
-    game.addDelay(500);
+    //game.addDelay(500);
     System.out.println(game.player);
-  } // End of main
+  }
 
   /* Instance Methods */
-  private Player start(Scanner console) {
-    // Add start functionality here
+  private String getSaveFile() {
+    return player.getName() + ".svr";
+  }
 
-    return new Player("Test");
-  } // End of start
+  private Player start(Scanner console) {
+    Player player = null;
+    homeScreen();
+
+    System.out.println("would you like to load a saved game? y/n");
+
+    while (player == null) {
+      String userInput = console.nextLine();
+      if (userInput.equals("y")) {
+        System.out.println("Enter the name you used for the game you want loaded");
+        player = load(console.nextLine(), console);
+      } else if (userInput.equals("n")) {
+        System.out.print("Enter your name : ");
+        userInput = console.nextLine();
+        player = new Player(userInput);
+      }
+    }
+    return player;
+  }
 
   private void save() {
-    // Add save functionality here
-
-  } // End of save
+    String fileName = getSaveFile();
+    try {
+      FileOutputStream saveFile = new FileOutputStream(fileName);
+      ObjectOutputStream playerStream = new ObjectOutputStream(saveFile);
+      playerStream.writeObject(player);
+    } catch (IOException e) {
+      System.out.printf("IOException: could not save game to file %s%n", player.getName());
+    }
+  }
 
   private Player load(String playerName, Scanner console) {
-    // Add load functionality here
-
-    return new Player("Test");
-  } // End of load
-
-  // Adds a delay to the console so it seems like the computer is "thinking"
+    Player loadedPlayer;
+    try {
+      String filename = playerName + ".svr";
+      FileInputStream saveFile = new FileInputStream(filename);
+      ObjectInputStream saveReader = new ObjectInputStream(saveFile);
+      loadedPlayer = (Player) saveReader.readObject();
+    } catch (Exception e) {
+      System.out.println("Error loading save file, new file will be created");
+      loadedPlayer = new Player(playerName);
+    }
+    return loadedPlayer;
+  }
+}
+/*
+  // Adds a delay to the console, so it seems like the computer is "thinking"
   // or "responding" like a human, not instantly like a computer.
   private void addDelay(int time) {
     try {
@@ -69,4 +106,4 @@ public class MedievalGame {
       e.printStackTrace();
     }
   }
-}
+}*/
